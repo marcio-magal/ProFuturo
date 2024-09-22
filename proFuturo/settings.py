321ad744bbe1acf9e10e86cad13a9ab6b1f3e564
marcio-magal <marcio.magalhaes.432@gmail.com>
2024-09-22 15:07:44 -0300

    Arquivos estáticos

--- proFuturo/settings.py
+++ proFuturo/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -111,6 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
