08b8b2dade53753a31bf64a02748ed968f9df100
marcio-magal <marcio.magalhaes.432@gmail.com>
2024-10-17 11:10:59 -0300

    2a Entrega

--- home/views.py
+++ home/views.py
@@ -1,6 +1,21 @@
+from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from playList.models import Playlist, Video
 
 @login_required
 def index(request):
-    return render(request, 'index.html')
+    # Ordene os vídeos por uma coluna específica, como 'id', 'created_at' ou 'title'.
+    video_list = Video.objects.all().order_by('id')
+    playlists = Playlist.objects.all()
+
+    # Crie o objeto Paginator e obtenha os vídeos da página solicitada
+    paginator = Paginator(video_list, 12)  # Carregar 12 vídeos por página
+    page_number = request.GET.get('page')
+    videos = paginator.get_page(page_number)
+
+    return render(request, 'index.html', {'playlists': playlists, 'videos': videos})
+
+@login_required
+def adm(request):
+    return render(request, 'adm.html')
--- playList/admin.py
+++ playList/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Video, Playlist, PlaylistVideo
 
-# Register your models here.
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'created_at')
+    search_fields = ('title', 'url')
+    list_filter = ('created_at',)
+
+@admin.register(Playlist)
+class PlaylistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('created_at',)
+
+@admin.register(PlaylistVideo)
+class PlaylistVideoAdmin(admin.ModelAdmin):
+    list_display = ('playlist', 'video', 'order')
+    list_filter = ('playlist',)
